e06b850ffa079460d1c965931f470f81e96895cd
Franco Balich <franco.balich@gmail.com>
2026-07-11 23:55:59 -0300

    content(talks): add Café Tecnológico UAI panel

--- data/talks.ts
+++ data/talks.ts
@@ -12,5 +12,15 @@ export interface Talk {
 }
 
 export const talks: Talk[] = [
-  // Completar con charlas y eventos reales
+  {
+    id: "cafe-tecnologico-uai-2026",
+    title: "Codeá tu futuro: tu primer empleo en tecnología",
+    event: "Café Tecnológico - UAI",
+    role: "panelist",
+    date: "2026-05-19",
+    location: "UAI, Buenos Aires",
+    eventUrl:
+      "https://www.linkedin.com/posts/alumnos-y-graduados-fti-uai_cafaeztecnolaejgico-ftiuai-alumnosuai-activity-7461061824290107392-jGUG",
+    upcoming: false,
+  },
 ];
